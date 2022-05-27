a9290aef6f03ebaf9e80380e239ef3f4ae5ce6dc
Дмитрий Цебинога <tsebik@ukr.net>
2022-05-27 22:35:09 +0300

    решение второй задачи

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+
+ingredients.forEach((el) => {
+  const li = document.createElement("li");
+  li.textContent = el;
+  li.classList.add("item");
+  ul.append(li);
+});
